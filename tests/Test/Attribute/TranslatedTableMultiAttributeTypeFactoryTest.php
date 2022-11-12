6300cdee8294f1e8f90ab3db4dcb8d5b2ba811f5
Ingolf Steinhardt <info@e-spin.de>
2022-11-12 21:28:44 +0100

    Fix gactions

--- tests/Test/Attribute/TranslatedTableMultiAttributeTypeFactoryTest.php
+++ tests/Test/Attribute/TranslatedTableMultiAttributeTypeFactoryTest.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_translatedtablemulti.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,8 @@
  * @subpackage AttributeTableMulti
  * @author     Andreas Dziemba <adziemba@web.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedtablemulti/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -30,6 +31,8 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * Test the attribute factory.
+ *
+ * @covers \MetaModels\AttributeTranslatedTableMultiBundle\Attribute\AttributeTypeFactory
  */
 class TranslatedTableMultiAttributeTypeFactoryTest extends TestCase
 {
