d96dfa4652c2f54665a0aebff424ecc5659cd4a0
Ingolf Steinardt <info@e-spin.de>
2024-07-20 14:54:18 +0200

    Changes for translation loader

--- src/Resources/contao/dca/tl_metamodel_rendersetting.php
+++ src/Resources/contao/dca/tl_metamodel_rendersetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_translatedtablemulti.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2024 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,24 +17,23 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     David Greminger <david.greminger@1up.io>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2024 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedtablemulti/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['metapalettes']['translatedtablemulti extends default'] = array
-(
-    '+advanced' => array('translatedtablemulti_hide_tablehead'),
-);
+$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['metapalettes']['translatedtablemulti extends default'] = [
+    '+advanced' => ['translatedtablemulti_hide_tablehead'],
+];
 
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['fields']['translatedtablemulti_hide_tablehead'] = array
-(
-    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['translatedtablemulti_hide_tablehead'],
-    'exclude'   => true,
-    'inputType' => 'checkbox',
-    'eval'      => array
-    (
+$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['fields']['translatedtablemulti_hide_tablehead'] = [
+    'label'       => 'translatedtablemulti_hide_tablehead.label',
+    'description' => 'translatedtablemulti_hide_tablehead.description',
+    'exclude'     => true,
+    'inputType'   => 'checkbox',
+    'eval'        => [
         'tl_class' => 'clr w50'
-    ),
-    'sql'       => "varchar(1) NOT NULL default '0'",
-);
+    ],
+    'sql'         => "varchar(1) NOT NULL default '0'",
+];
